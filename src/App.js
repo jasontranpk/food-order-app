2ef6c677e7d5948ede934924a34152828922f90f
Jason Tran <jasontran.pk@gmail.com>
2022-07-07 12:16:51 +0700

    manage cart and modal state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useState } from 'react';
+
 import Header from './Layout/Header';
 import Meals from './Meals/Meals';
+import Cart from './Cart/Cart';
+
 function App() {
+	const [cartIsShown, setCartIsShown] = useState(false);
+
+	function showCartHandler() {
+		setCartIsShown(true);
+	}
+	function hideCartHandler() {
+		setCartIsShown(false);
+	}
 	return (
 		<>
-			<Header />
-			<Meals />
+			{cartIsShown && <Cart onClose={hideCartHandler} />}
+
+			<Header onShowCart={showCartHandler} />
+			<main>
+				<Meals />
+			</main>
 		</>
 	);
 }
