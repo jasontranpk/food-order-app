3447fdd27f7b0e7efdef5b880069a726c77002be
Jason Tran <jasontran.pk@gmail.com>
2022-07-07 10:52:21 +0700

    add Meals Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Header from './Layout/Header';
-
+import Meals from './Meals/Meals';
 function App() {
 	return (
 		<>
 			<Header />
+			<Meals />
 		</>
 	);
 }
---
+++ src/Meals/Meals.js
@@ -0,0 +1,12 @@
+import AvailableMeal from './AvailableMeals';
+import MealsSummary from './MealsSummary';
+function Meals() {
+	return (
+		<>
+			<MealsSummary />
+			<AvailableMeal />
+		</>
+	);
+}
+
+export default Meals;
