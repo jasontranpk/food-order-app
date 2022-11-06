c638b0ddc4c43c2c23b601f2c07e018c16f774e0
Trần Sơn <62680684+jasontranpk@users.noreply.github.com>
2022-11-06 15:26:19 +0700

    Update Modal.js

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -1,6 +1,7 @@
 import classes from './Modal.module.css';
 import ReactDOM from 'react-dom';
 
+//Modal
 function Backdrop(props) {
 	return <div className={classes.backdrop} onClick={props.onClose}></div>;
 }
